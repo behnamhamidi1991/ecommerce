dfca698a9adceb762a0da5d5f57c97bf20d4cd47
behnamhamidi1991 <eslp91@hotmail.com>
2024-01-14 14:36:44 +0330

    Setup packages

---
+++ src/utils/data.js
@@ -0,0 +1,34 @@
+export const data = {
+  products: [
+    {
+      id: "1",
+      name: "Espresso",
+      image: "/p1.jpg",
+      price: 25,
+      countInStock: 10,
+      rating: 4.5,
+      numReviews: 10,
+      description: "High Quality Coffee",
+    },
+    {
+      id: "2",
+      name: "Latte",
+      image: "/p2.jpg",
+      price: 15,
+      countInStock: 10,
+      rating: 4.5,
+      numReviews: 8,
+      description: "Material from Italy",
+    },
+    {
+      id: "3",
+      name: "Cupoccino",
+      image: "/p3.jpg",
+      price: 22,
+      countInStock: 10,
+      rating: 6,
+      numReviews: 10,
+      description: "This comes from Brazil",
+    },
+  ],
+};
