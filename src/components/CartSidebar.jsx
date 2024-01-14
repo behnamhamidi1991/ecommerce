4069655bf7908ef26cfcd9ed9142f2a0ce293257
behnamhamidi1991 <eslp91@hotmail.com>
2024-01-14 15:50:41 +0330

    Add redux to the project

--- src/components/CartSidebar.jsx
+++ src/components/CartSidebar.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function CartSidebar() {
   return (
-    <div className="fixed top-0 right-0 w-32 h-full shadow-lg border-l border-l-gray-700 overflow-scroll">
-      <div className="py-5 px-2">Cart is empty</div>
+    <div className="fixed top-0 right-0 w-38 h-full shadow-lg border-l border-l-gray-700 overflow-scroll">
+      <div className="py-5 px-1">Cart is empty</div>
     </div>
   );
 }
